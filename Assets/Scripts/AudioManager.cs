b835ad0824d530330d606a4437c5579716bf3ad6
pretty_wired <nasirhafsa705@gmail.com>
2024-08-02 11:44:45 +0500

    added music in the main menu

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Android.Types;
+using UnityEngine;
+using System;
+using Unity.VisualScripting;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public SoundClass[] musicSounds;
+    public AudioSource musicSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Start()
+    {
+        PlayMusic("Theme");
+    }
+    public void PlayMusic(string name)
+    {
+        SoundClass s = Array.Find(musicSounds, x => x.songName == name);
+        if (s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+        else
+        {
+            musicSource.clip = s.clip;
+            musicSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/SoundClass.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class SoundClass : MonoBehaviour
+{
+    public string songName;
+    public AudioClip clip;
+}
