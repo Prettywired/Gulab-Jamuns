099b3ec1c5f9ebf7494ca809a2cc858aa6d18445
pretty_wired <nasirhafsa705@gmail.com>
2024-08-20 12:41:32 +0500

    added rewarded admobs

--- Assets/Scripts/PauseMenuScript.cs
+++ Assets/Scripts/PauseMenuScript.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenuScript : MonoBehaviour
 {
+    float delay = 1f;
+    public AdManager adManager;
     public void Home()
     {
         SceneManager.LoadScene(0);
     }
     public void Restart()
     {
+        adManager.ShowRewardedAd();
+        StartCoroutine(HandleRestart());
+
+
+    }
+    IEnumerator HandleRestart()
+    {
+        yield return new WaitForSeconds(delay);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         Stars.ResetMistakes();
     }
